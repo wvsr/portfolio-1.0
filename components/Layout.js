835cd81e621b81d276f5451624af3e555137120f
Sami <email.wasimul@gmail.com>
2022-11-03 00:05:24 +0600

    add meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,19 @@ function Layout({ children }) {
   return (
     <div>
       <Head>
+        <meta name='title' content='Wasimul Vari Sami - Web Programmer' />
+        <meta
+          name='description'
+          content='A Full Stack Developer who specializes in JavaScript and python Technologies across the whole mern stack (Node.js, Express, Angular, React). Experience building complete web applications with backend API System'
+        />
+        <meta
+          name='keywords'
+          content='Wasimul Sami, Wasimul Vari Sami, Developer Portfolio'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='language' content='English' />
+
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
         <link
